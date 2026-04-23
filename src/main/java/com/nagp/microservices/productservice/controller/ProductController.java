8693c35a7ba180ee4ee77d7435efd84e8cf44805
samyak1903 <samyakjain1903@gmail.com>
2026-04-24 01:11:58 +0530

    Fixed Ivalid CORS Issue

--- src/main/java/com/nagp/microservices/productservice/controller/ProductController.java
+++ src/main/java/com/nagp/microservices/productservice/controller/ProductController.java
@@ -13,7 +13,8 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://d2emlgpiz49p5b.cloudfront.net")
 public class ProductController {
 
 	private final ProductRepository productRepository;
